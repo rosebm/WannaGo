b31dd97f16eace1b855f451a283cc0e173f8e52f
Rosalyn <rosebm@users.noreply.github.com>
2021-04-07 20:16:33 -0400

    Update III:
    - updating gitignore

---
+++ app/src/main/java/com/rosalynbm/wannago2/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.rosalynbm.wannago2.util
+
+object Constants {
+    const val BASE_URL = "https://maps.googleapis.com/maps/api/place/"
+    const val API_KEY = "AIzaSyCMOu7Vr6OMk407tD5cIEYe_eGy7MGXdxs"
+}
